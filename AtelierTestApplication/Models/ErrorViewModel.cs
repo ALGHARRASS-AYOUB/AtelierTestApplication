0c734fd9e3a826bec0dd9f523c27ccc83d332d10
ALGHARRASS-AYOUB <ALGHARRASS.ayoub@gmail.com>
2023-07-04 13:27:42 +0100

    first commit

---
+++ AtelierTestApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AtelierTestApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
